5adb4212e80d871697086f64c7b83ad982cade5c
Rafah <irafah.alves@gmail.com>
2025-10-31 12:07:44 -0300

    HHero: FE FileUpload component created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
-import { UploadCloud, FileText } from 'lucide-react'
+import { GiRobotHelmet } from "react-icons/gi";
+import { FaMagnifyingGlass } from "react-icons/fa6";
+
+
 
 import './App.css'
 import FormGroup from './components/FormGroup.jsx'
+import FileUpload from './components/FileUpload.jsx'
 
 function App() {
 
   const [jobTitle, setJobTitle] = useState('');
   const [jobDescription, setJobDescription] = useState('');
-  const [file, setFile] = useState(null);
+  const [files, setFiles] = useState([]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -18,83 +22,68 @@ function App() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center px-4">
-      <div className="w-full max-w-2xl bg-white rounded-2xl shadow-lg p-8 space-y-6">
-        <div className="text-center">
-          <h1 className="text-3xl font-bold text-gray-800 flex items-center justify-center gap-2">
-            CV Analyst <FileText className="w-6 h-6 text-gray-600" />
-          </h1>
-          <p className="text-gray-500 mt-2">
-            See how well your CV matches a job description — instantly.
-          </p>
+
+    <main className="min-h-screen bg-">
+      <header className="text-center p-10">
+        <h1 className="text-4xl font-bold text-white-800 flex items-center justify-center gap-2">
+          HR Hero <GiRobotHelmet /> |
+        </h1>
+        <p className="text-white-500 mt-2 ml-5">
+          Your AI-powered assistant for CV analysis and job matching.
+        </p>
+      </header>
+
+      <div className="flex items-center justify-center px-4 mt-15">
+        <div className="w-full max-w-2xl bg-white rounded-2xl shadow-lg p-8 space-y-6">
+          <form className="space-y-5">
+            <div>
+              {/* Job title */}
+              <FormGroup>
+                <label className="block text-sm font-medium text-gray-700 mb-2">
+                  Job title
+                </label>
+                <input
+                  type="text"
+                  id="jobTitle"
+                  placeholder="e.g., Full-Stack Developer"
+                  className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:ring-2 focus:ring-blue-500 focus:outline-none"
+                  value={jobTitle}
+                  onChange={(e) => setJobTitle(e.target.value)}
+                />
+              </FormGroup>
+            </div>
+
+            {/* Job description */}
+            <div>
+              <FormGroup>
+                <label className="block text-sm font-medium text-gray-700 mb-1">
+                  Job description
+                </label>
+                <textarea
+                  placeholder="Paste the job description here"
+                  rows="5"
+                  className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:ring-2 focus:ring-blue-500 focus:outline-none"
+                  value={jobDescription}
+                  onChange={(e) => setJobDescription(e.target.value)}
+                ></textarea>
+              </FormGroup>
+            </div>
+
+            {/* File upload */}
+            <FileUpload onFilesSelected={setFiles} />
+
+            {/* Submit */}
+            <button
+              type="submit"
+              className="w-full bg-green-600 text-white font-medium py-2.5 rounded-lg hover:bg-green-700 transition cursor-pointer"
+            >
+              Analyse CV
+              <FaMagnifyingGlass className="inline-block ml-2" />
+            </button>
+          </form>
         </div>
-
-        <form className="space-y-5">
-          <div>
-            {/* Job title */}
-            <FormGroup>
-              <label className="block text-md font-medium text-gray-700 mb-1">
-                Job title
-              </label>
-              <input
-                type="text"
-                id="jobTitle"
-                placeholder="e.g., Full-Stack Developer"
-                className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:ring-2 focus:ring-blue-500 focus:outline-none"
-                value={jobTitle}
-                onChange={(e) => setJobTitle(e.target.value)}
-              />
-            </FormGroup>
-          </div>
-
-          {/* Job description */}
-          <div>
-            <FormGroup>
-              <label className="block text-sm font-medium text-gray-700 mb-1">
-                Job description
-              </label>
-              <textarea
-                placeholder="Paste the job description here"
-                rows="5"
-                className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:ring-2 focus:ring-blue-500 focus:outline-none"
-                value={jobDescription}
-                onChange={(e) => setJobDescription(e.target.value)}
-              ></textarea>
-            </FormGroup>
-          </div>
-
-          {/* File upload */}
-          <div className="border-2 border-dashed border-gray-300 rounded-lg p-6 flex flex-col items-center justify-center text-center hover:border-blue-400 transition m-5">
-            <UploadCloud className="w-10 h-10 text-gray-400" />
-            <p className="mt-2 text-gray-500">
-              Drag & drop your CV (PDF) or{" "}
-              <label
-                htmlFor="file-upload"
-                className="text-blue-600 font-medium cursor-pointer hover:underline"
-              >
-                browse files
-              </label>
-            </p>
-            <input
-              id="file-upload"
-              type="file"
-              accept="application/pdf"
-              className="hidden"
-              onChange={(e) => setFile(e.target.files[0])}
-            />
-            {file && <p className="mt-2 text-sm text-gray-600">{file.name}</p>}
-          </div>
-
-          {/* Submit */}
-          <button
-            type="submit"
-            className="w-full bg-blue-600 text-white font-medium py-2.5 rounded-lg hover:bg-blue-700 transition"
-          >
-            Analyse CV
-          </button>
-        </form>
       </div>
-    </div>
+    </main>
   )
 }
 
